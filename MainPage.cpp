4d18bdff974238e9e3f698476d28e8bf8252f020
An Vu <104729655+AnVuGia@users.noreply.github.com>
2022-05-18 15:43:57 +0700

    finish guest_route

--- MainPage.cpp
+++ MainPage.cpp
@@ -9,6 +9,8 @@ using std::string;
 using std::vector;
 using std::stringstream;
 Global program;
+void guest_route();
+void member_route();
 void print_header(){
     cout << "\n\nEEET2482/COSC2082 ASSIGNMENT\n";
     cout << "VACATION HOUSE EXCHANGE APPLICATION";
@@ -19,7 +21,7 @@ void print_header(){
         << "s, student 2" <<"\n"
         << "s, student 3" <<"\n";
 }
-void user_choice(){
+void layer_1(){
     int choice;
     cout << "\nUse the app as: 1. Guest  2. Member   3.  Admin\n";
     cout << "Enter your choice: ";
@@ -27,7 +29,7 @@ void user_choice(){
     switch (choice)
     {
     case 1:
-        showHouse(&program); // test câu 2
+        guest_route(); // test câu 2
         break;
         // program.user_register();
         // break;
@@ -39,24 +41,57 @@ void user_choice(){
         break;
     }
 }
-int main(){
+void guest_route(){
+    int choice;
+    cout << "You are a guest!\n";
+    cout<<"0. Exit\n";
+    cout << "1. Show info (shortened version): ";
+    cout <<"2. Register";
+    choice = program.choice();
+    switch (choice)
+    {
+    case 1:
+        int choice_1;
+        for(auto user: program.users){
+            user.get_own_house().show_short_info();
+        }
+        cout <<"0. Back\n";
+        cout <<"1. Register\n";
+        choice_1 = program.choice();
+        if(choice_1 == 0){
+            layer_1();
+        } else if(choice_1 == 1){
+            program.user_register();
+            member_route();
+        } else{
+            return;
+        }
+        break;
+    case 2:
+        program.user_register();
+        member_route();
+        break;
+    case 0:
+        cout << "Exit!";
+        return;
+    default:
+        cout << "Not valid!";
+        break;
+    }
+    
+}
+void member_route(){
+    cout <<"in member route";
+}
+void start(){ //on start
     program.inputData();
     setup(&program);
     print_header();
-    // program.user_register();
-    user_choice();
-    
-    // accept_request(program.CurrentUser, &program); //test request accept & fees exchange between users
-    rate_occupier(program.CurrentUser->get_occupier());   //test rate occupier
-    // rate_occupier(program.CurrentUser);  //test avg rating
-    // program.CurrentUser->show_user_ratings();
-    rate_house(program.CurrentUser->get_occupying()); //test rate house
-    // rate_house(program.CurrentUser); //test avg rating
-    // // program.CurrentUser->show_info();
-    // program.CurrentUser->show_house_ratings();
-    // find_suitable_house(program.CurrentUser, &program);
-    // program.CurrentUser->show_requests();
-    program.end();
+    layer_1();
+}
 
+int main(){
+    start();
+    program.end();
     return 0;
 }
